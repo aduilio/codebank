531b152ceae1331e10c2006c0293d4dbbff4d176
Aduilio Silva <developer.aduilio@gmail.com>
2022-08-19 07:43:12 -0300

    Add gPRC dependencies
    - Create the gRPC infraestructure.
    - Create a Makefile to build the proto files.

--- codebank/go.mod
+++ codebank/go.mod
@@ -4,6 +4,12 @@ go 1.15
 
 require (
 	github.com/confluentinc/confluent-kafka-go v1.9.2
+	github.com/golang/protobuf v1.5.2
 	github.com/lib/pq v1.10.6
 	github.com/satori/go.uuid v1.2.0
+	golang.org/x/net v0.0.0-20220812174116-3211cb980234 // indirect
+	golang.org/x/sys v0.0.0-20220818161305-2296e01440c6 // indirect
+	google.golang.org/genproto v0.0.0-20220817144833-d7fd3f11b9b1 // indirect
+	google.golang.org/grpc v1.48.0
+	google.golang.org/protobuf v1.28.1
 )
---
+++ codebank/infrastructure/grpc/server/server.go
@@ -0,0 +1,35 @@
+package server
+
+import (
+	"fmt"
+	"net"
+
+	"github.com/aduilio/codebank/infrastructure/grpc/pb"
+	"github.com/aduilio/codebank/infrastructure/grpc/service"
+	"github.com/aduilio/codebank/usecase"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+type GRPCServer struct {
+	ProcessTransactionUseCase usecase.UseCaseTransaction
+}
+
+func NewGRPCServer() GRPCServer {
+	return GRPCServer{}
+}
+
+func (s GRPCServer) Serve() {
+	listener, err := net.Listen("tcp", "0.0.0.0:50051")
+	if err != nil {
+		fmt.Println("Could not run the server")
+	}
+	transactionService := service.NewTransactionService()
+	transactionService.ProcessTransactionUseCase = s.ProcessTransactionUseCase
+
+	grpcServer := grpc.NewServer()
+	reflection.Register(grpcServer)
+	pb.RegisterPaymentServiceServer(grpcServer, transactionService)
+
+	grpcServer.Serve(listener)
+}
