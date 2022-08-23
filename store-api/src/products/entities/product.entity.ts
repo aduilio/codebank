7eb8db056b31a1de59ac6d492161adfe4a247ebc
Aduilio Silva <developer.aduilio@gmail.com>
2022-08-23 16:38:24 -0300

    Add TypeORM module
    - Adjust the entity to reflect the database table.
    - Create the migrations files.

--- store-api/src/products/entities/product.entity.ts
+++ store-api/src/products/entities/product.entity.ts
@@ -1,1 +1,26 @@
-export class Product {}
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity({name: 'products'})
+export class Product {
+
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column()
+    name: string
+
+    @Column()
+    description: string
+
+    @Column()
+    image_urm: string
+
+    @Column()
+    slug: string
+
+    @Column()
+    price: number
+
+    @CreateDateColumn({type: 'timestamp'})
+    created_at: Date;
+}
--- store-api/src/products/products.service.ts
+++ store-api/src/products/products.service.ts
@@ -1,26 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { Product } from './entities/product.entity';
 
 @Injectable()
 export class ProductsService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
-  }
 
-  findAll() {
-    return `This action returns all products`;
-  }
+    constructor(@InjectRepository(Product) private prodsuctRepo: Repository<Product>) {}
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
-  }
+    create(createProductDto: CreateProductDto) {
+        return 'This action adds a new product';
+    }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
-  }
+    findAll() {
+        return `This action returns all products`;
+    }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
-  }
+    findOne(id: number) {
+        return `This action returns a #${id} product`;
+    }
+
+    update(id: number, updateProductDto: UpdateProductDto) {
+        return `This action updates a #${id} product`;
+    }
+
+    remove(id: number) {
+        return `This action removes a #${id} product`;
+    }
 }
